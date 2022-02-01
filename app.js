3ba0ad6dbfce23d4fbbe5913f97655bf4c27e683
unknown <lipesantossimao@gmail.com>
2022-01-31 22:19:34 -0200

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+
+var img = document.querySelector('.banner img')
+
+function Omen(){
+    img.src = 'https://static.valorantzone.gg/news/2020/04/27121303/omen1.png'
+}
+
+function Sage(){
+    img.src = 'https://i.pinimg.com/originals/30/37/8a/30378af6c20365cdaaf5a6cb306430e7.png'
+}
+
+function Sova(){
+    img.src = 'https://static.valorantzone.gg/news/2020/06/02170318/reyna-guia-valorant-439x1024.png'
+}
